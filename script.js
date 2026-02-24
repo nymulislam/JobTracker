aa4f7c9e0c6da7bcb2778bcfc860561a55522024
nymulislam <naymulislam241@gmail.com>
2026-02-24 09:29:54 +0600

    Refactored: Improvement Overall rendaring logic.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let allJobs = [];
 let jobStatus = [];
 
 const loadJobs = () => {
@@ -5,100 +6,101 @@ const loadJobs = () => {
         .then(res => res.json())
         .then(data => {
             allJobs = data;
-            displayJobs(data)
+            displayJobs(allJobs)
         })
 }
 
-const displayJobs = (jobs) => {
-    const jobsContainer = document.getElementById("jobs-container")
+document.addEventListener('click', function (e) {
+    const interview = e.target.closest(".interview-btn")
+    const reject = e.target.closest(".reject-btn")
+    const tabBtn = e.target.closest(".tab-btn")
 
-    // Display No-Jobs card conditionally
-    const noJobsCard = document.getElementById("no-jobs");
-    if (jobs.length === 0) {
-        noJobsCard.classList.remove('hidden')
+    // Change card status
+    if (interview || reject) {
+        const jobCard = e.target.closest('.job-card')
+        const jobId = Number(jobCard.dataset.id)
+
+        let existingJob = jobStatus.find(j => j.id === jobId)
+
+        if (!existingJob) {
+            const job = allJobs.find(j => j.id === jobId)
+            jobStatus.push(job)
+            existingJob = jobStatus.find(j => j.id === jobId)
+        }
+
+        existingJob.status = interview ? "Interview" :
+            "Reject"
+
+        UpdateCounts()
+        displayCurrentTab()
     }
 
-    document.addEventListener('click', function (e) {
-        const tabBtn = e.target.closest(".tab-btn")
-        const interview = e.target.closest(".interview-btn")
-        const reject = e.target.closest(".reject-btn")
-
-        // Change card status
-        if (interview || reject) {
-            const jobCard = e.target.closest('.job-card')
-            const jobId = Number(jobCard.dataset.id)
-            const job = jobs.find(j => j.id === jobId)
-
-            let existingJob = jobStatus.find(j => j.id === jobId)
-
-            if (!existingJob) {
-                jobStatus.push(job)
-                existingJob = job
-            }
-
-            // Update Status
-            if (interview) {
-                existingJob.status = 'Interview'
-            }
-            if (reject) {
-                existingJob.status = 'Reject'
-            }
-
-            const interviewed = jobStatus.filter(j => j.status === 'Interview')
-            const rejected = jobStatus.filter(j => j.status === 'Reject')
-
-            document.getElementById("interview-count").innerText = interviewed.length;
-            document.getElementById("reject-count").innerText = rejected.length;
-
-            // Change status on behavior
-            const changeStatus = jobCard.querySelector('.change-status');
-
-            if (existingJob.status === 'Interview') {
-                changeStatus.innerText = `INTERVIEW`
-                changeStatus.classList.add('text-green-500', 'bg-green-100', 'border-l-3', 'font-bold')
-                changeStatus.classList.remove('text-red-500', 'bg-red-100')
-            }
-            if (existingJob.status === 'Reject') {
-                changeStatus.innerText = `REJECTED`
-                changeStatus.classList.add('text-red-500', 'bg-red-100', 'border-l-3', 'font-bold')
-                changeStatus.classList.remove('text-green-500', 'bg-green-100')
-            }
-        }
-
-        // Tab btn change conditionally
-        if (!tabBtn) return;
+    // Tab btn change conditionally
+    if (!tabBtn) return;
+    if (tabBtn) {
         document.querySelectorAll(".tab-btn").forEach(btn => btn.classList.remove("btn-active"))
         tabBtn.classList.add("btn-active");
-        if (tabBtn.innerText === 'All') {
-            if (jobs.length === 0) {
-                noJobsCard.classList.remove('hidden')
-            } else {
-                noJobsCard.classList.add('hidden')
-                loadJobs()
-            }
-        }
-        if (tabBtn.innerText === 'Interview') {
-            jobsContainer.innerHTML = " ";
-            if (interviewed.length === 0) {
-                noJobsCard.classList.remove('hidden')
-            }
-        }
-        if (tabBtn.innerText === 'Rejected') {
-            jobsContainer.innerHTML = " ";
-            if (rejectedJobs.length === 0) {
-                noJobsCard.classList.remove('hidden')
-            }
-        }
-    })
 
-    // Display total jobs count
-    document.getElementById("total-jobs").innerText = jobs.length;
-    document.getElementById("available-jobs").innerText = jobs.length;
+        displayCurrentTab();
+    }
 
+})
 
+// Load data click on tab
+function displayCurrentTab() {
+    const activeTab = document.querySelector(".tab-btn.btn-active").innerText
+
+    if (activeTab === "All") {
+        displayJobs(allJobs)
+    }
+    if (activeTab === "Interview") {
+        const interviewed = jobStatus.filter(j => j.status === 'Interview')
+        displayJobs(interviewed)
+    }
+    if (activeTab === "Rejected") {
+        const rejected = jobStatus.filter(j => j.status === 'Reject')
+        displayJobs(rejected)
+    }
+}
+
+// update count
+function UpdateCounts() {
+    const interviewed = jobStatus.filter(j => j.status === "Interview");
+    const rejected = jobStatus.filter(j => j.status === "Reject");
+
+    document.getElementById("interview-count").innerText = interviewed.length
+    document.getElementById("reject-count").innerText = rejected.length
+
+}
+// Display all jobs data
+const displayJobs = (jobs) => {
+    const jobsContainer = document.getElementById("jobs-container")
+    const noJobsCard = document.getElementById("no-jobs").classList;
+    document.getElementById("total-jobs").innerText = allJobs.length
     jobsContainer.innerHTML = "";
+
+    jobs.length !== 0 ?
+        noJobsCard.add('hidden') :
+        noJobsCard.remove('hidden')
+
     for (const job of jobs) {
         const jobsCard = document.createElement("div")
+
+        const statusCheck = jobStatus.find(j => j.id === job.id)
+
+        const statusText = statusCheck ? statusCheck.status :
+            "NOT APPLIED"
+
+        let statusClass = ""
+        if (statusText === "Interview") {
+            statusClass = "text-green-500 bg-green-100 border-l-3 font-bold"
+        }
+
+        if (statusText === "Reject") {
+            statusClass = "text-red-500 bg-red-100 border-l-3 font-bold"
+        }
+
+
         jobsCard.innerHTML = `
         <div data-id=${job.id} class="job-card space-y-3 shadow-sm p-8 rounded-md">
                     <div class="flex justify-between">
@@ -115,7 +117,7 @@ const displayJobs = (jobs) => {
                         <li>${job.type}</li>
                         <li>${job.salary}</li>
                     </ol>
-                    <h3 class="px-4 py-2 mt-3 shadow-sm w-fit rounded-md change-status">NOT APPLIED</h3>
+                    <h3 class="px-4 py-2 mt-3 shadow-sm w-fit rounded-md change-status ${statusClass}">${statusText}</h3>
                     <p>${job.description}</p>
                     <div class="space-x-4">
                         <button class="btn btn-success btn-soft interview-btn shadow-sm">
